2b43673021b02d8230fa8749c5bff4aba31be0cd
Grenysherg <grenysherg@gmail.com>
2017-09-20 18:40:41 +0300

    Исправление ошибок Edge

--- js/node-children-polyfill.js
+++ js/node-children-polyfill.js
@@ -1,26 +1,77 @@
 'use strict';
 
-(function (constructor) {
-  if (constructor &&
-    constructor.prototype &&
-    constructor.prototype.children === null) {
-    Object.defineProperty(constructor.prototype, 'children', {
-      get: function () {
-        var i = 0;
-        var nodes = this.childNodes;
-        var node = nodes[i++];
-        var children = [];
-
-        while (node) {
-          if (node.nodeType === 1) {
-            children.push(node);
+(function () {
+  var polyfill = function (prototype) {
+    if (!('firstElementChild' in prototype)) {
+      Object.defineProperty(prototype, 'firstElementChild', {
+        get: function () {
+          var nodes = this.childNodes;
+          var length = nodes.length;
+          var current;
+          for (var i = 0; i < length; i++) {
+            current = nodes[i];
+            if (current.nodeType === 1) {
+              return current;
+            }
           }
-
-          node = nodes[i++];
+          return null;
         }
+      });
+    }
 
-        return children;
-      }
-    });
-  }
-})(window.Node || window.Element);
+    if (!('lastElementChild' in prototype)) {
+      Object.defineProperty(prototype, 'lastElementChild', {
+        get: function () {
+          var nodes = this.childNodes;
+          var length = nodes.length;
+          var current;
+          for (var i = length - 1; i > 0; i--) {
+            current = nodes[i];
+            if (current.nodeType === 1) {
+              return current;
+            }
+          }
+          return null;
+        }
+      });
+    }
+
+    if (!('childElementCount' in prototype)) {
+      Object.defineProperty(prototype, 'childElementCount', {
+        get: function () {
+          var nodes = this.childNodes;
+          var length = nodes.length;
+          var count = 0;
+          for (var i = 0; i < length; i++) {
+            if (nodes[i].nodeType === 1) {
+              count++;
+            }
+          }
+          return count;
+        }
+      });
+    }
+
+    if (!('children' in prototype)) {
+      Object.defineProperty(prototype, 'children', {
+        get: function () {
+          var nodes = this.childNodes;
+          var length = nodes.length;
+          var children = [];
+          var current;
+          for (var i = 0; i < length; i++) {
+            current = nodes[i];
+            if (current.nodeType === 1) {
+              children.push(current);
+            }
+          }
+          return children;
+        }
+      });
+    }
+  };
+
+  polyfill(Element.prototype);
+  polyfill(Document.prototype);
+  polyfill(DocumentFragment.prototype);
+})();
